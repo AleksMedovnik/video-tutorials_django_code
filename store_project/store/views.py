3a182ce24f5db468f1cee3f12664ba4631c76bf4
Alex <alex@a.com>
2023-05-12 11:57:23 +0300

    Added build_template to the view

--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 
+def build_template(lst: list, cols: int) -> list[list]:
+    return 
+
+
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9,], [10]]
+print(build_template([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+
+
 def product_list(request):
+    products = [
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price': 150000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price': 100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price': 250000},
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price': 150000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price': 100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price': 250000},
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price': 150000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price': 100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price': 250000},
+    ]
     return render(
         request,
         'store/product_list.html',
-        context={'product_title': 'Range Rover'}
+        context={'products': products}
     )
