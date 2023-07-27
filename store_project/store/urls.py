dde98d715a2535af87c963072e54a013acbf4cf5
Alex <alex@a.com>
2023-07-27 16:04:47 +0300

    Added queryset practic

--- store_project/store/urls.py
+++ store_project/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ProductView, CategoryView, save_order
+from .views import HomeView, ProductView, CategoryView, save_order, work
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='product_list_url'),
+    path('', work),
+    # path('', HomeView.as_view(), name='product_list_url'),
     path('product/<int:pk>/', ProductView.as_view(), name='product_detail_url'),
     path('category/<int:pk>/', CategoryView.as_view(), name='category_detail_url'),
     path('save_order', save_order),
--- store_project/store/views.py
+++ store_project/store/views.py
@@ -3,6 +3,49 @@ from .models import *
 from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from .utils import CategoriesMixin
+from django.http import HttpResponse
+
+
+def work(request):
+    # p = Product(title='Ford', price=20000)
+    # p.save()
+
+    # Product.objects.create(title='BMW', price=25000)
+
+    # obj = Product.objects.get(pk=10)
+    # obj.price += 5000
+
+    # Product.objects.filter(pk=10).update(price=30000)
+
+    # obj = Product.objects.get(pk=10).delete()
+
+    # obj = Product.objects.exclude(title='Ford')
+
+    # obj = Product.objects.all().order_by('-pk')
+
+    # obj = Product.objects.filter(title='Ford').count()
+
+    # obj = Product.objects.filter(title='Lada').exists()
+
+    # obj = Product.objects.filter(price__gt=100000) # >
+    # obj = Product.objects.filter(price__gte=100000) # >=
+    # obj = Product.objects.filter(price__lt=100000) # <
+    # obj = Product.objects.filter(price__lte=100000)  # <=
+    # obj = Product.objects.filter(price__in=(25000, 20000, 100000)) 
+
+    # obj = Product.objects.filter(title__exact='range RoveR Sport') 
+    # obj = Product.objects.filter(title__iexact='range RoveR Sport') 
+
+    # obj = Product.objects.filter(title__contains='Rover') 
+    # obj = Product.objects.filter(title__icontains='rovER') 
+
+    # o = Order.objects.select_related('product').get(pk=1)
+    # p = o.product
+
+    # p = Product.objects.prefetch_related('categories').get(pk=1)
+    # cats = p.categories.all()
+    
+    return HttpResponse('Hello')
 
 
 class HomeView(ListView, CategoriesMixin):
